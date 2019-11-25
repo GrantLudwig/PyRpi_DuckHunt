0b9710b59c7ba9dd54fde7a3785fdfdaafbb0967
Grant Ludwig <grantludwig97@gmail.com>
2019-11-24 21:14:22 -0800

    basic game working

---
+++ Duck.py
@@ -0,0 +1,102 @@
+from graphics import *
+import time
+import random
+
+class Duck:
+    # Variables
+    # ---------
+    # private Image DuckImage
+    # private int index // index in round of ducks
+    # private bool Alive
+    # private bool Active
+    # private Time ActiveTime
+    # private int TIME_TO_SHOOT
+    # private float TIME_TILL_NEW_VELOCITY
+    # private int VelocityX
+    # private int VelocityY
+    # private Time VelocitySetTime
+    
+    def __init__(self, DuckImage, Index, maxX, maxY):
+        self.__DuckImage = DuckImage
+        self.Index = Index
+        self.__Alive = True
+        self.__Active = False
+        self.__Activated = False
+        self.__ActiveTime = None
+        self.__TIME_TO_SHOOT = 6
+        self.__TIME_TILL_NEW_VELOCITY = 0.5
+        self.__MaxX = maxX
+        self.__MaxY = maxY
+        self.__VelocityX = random.randint(-maxX,maxX)
+        self.__VelocityY = random.randint(-maxY,maxY)
+        self.__VelocitySetTime = None
+        self.FlyingAway = False
+        
+    def getVelocityX(self):
+        if time.time() - self.__VelocitySetTime >= self.__TIME_TILL_NEW_VELOCITY:
+            # set VelocityX
+            self.__VelocityX = random.randint(-self.__MaxX,self.__MaxX)
+            while self.__VelocityX == 0:
+                self.__VelocityX = random.randint(-self.__MaxX,self.__MaxX)
+            # set VelocityY
+            self.__VelocityY = random.randint(-self.__MaxY,self.__MaxY)
+            while self.__VelocityY == 0:
+                self.__VelocityY = random.randint(-self.__MaxY,self.__MaxY)
+            # reset time
+            self.__VelocitySetTime = time.time()
+            return self.__VelocityX
+        else:
+            return self.__VelocityX
+            
+    def getVelocityY(self):
+        if time.time() - self.__VelocitySetTime >= self.__TIME_TILL_NEW_VELOCITY:
+            # set VelocityX
+            self.__VelocityX = random.randint(-self.__MaxX,self.__MaxX)
+            while self.__VelocityX == 0:
+                self.__VelocityX = random.randint(-self.__MaxX,self.__MaxX)
+            # set VelocityY
+            self.__VelocityY = random.randint(-self.__MaxY,self.__MaxY)
+            while self.__VelocityY == 0:
+                self.__VelocityY = random.randint(-self.__MaxY,self.__MaxY)
+            # reset time
+            self.__VelocitySetTime = time.time()
+            return self.__VelocityY
+        else:
+            return self.__VelocityY
+            
+    def getRawVelocityX(self):
+        return self.__VelocityX
+        
+    def getRawVelocityY(self):
+        return self.__VelocityY
+        
+    def setVelocityX(self, value):
+        self.__VelocityX = value
+        
+    def setVelocityY(self, value):
+        self.__VelocityY = value
+        
+    def duckGraphic(self):
+        return self.__DuckImage
+        
+    def isActive(self):
+        if time.time() - self.__ActiveTime >= self.__TIME_TO_SHOOT:
+            self.__Active = False
+        return self.__Active
+        
+    def isAlive(self):
+        return self.__Alive
+        
+    def isActivated(self):
+        return self.__Activated
+        
+    def setActive(self):
+        self.__ActiveTime = time.time()
+        self.__VelocitySetTime = time.time()
+        self.__Active = True
+        self.__Activated = True
+        
+    def killed(self):
+        self.__Alive = False
+        self.__Active = False
+        self.FlyingAway = False
--- duckHunt.py
+++ duckHunt.py
@@ -7,6 +7,7 @@
 # Duck Hunt
 
 from graphics import *
+from Duck import *
 import busio
 import digitalio
 import board
@@ -18,7 +19,7 @@ import math
 import time
 
 SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 480
+SCREEN_HEIGHT = 450
 
 kill = False
 
@@ -28,18 +29,46 @@ ducks = (   "/home/ludwigg/Python/PyRpi_DuckHunt/blueDuck.png",
 deadDucks = (   "/home/ludwigg/Python/PyRpi_DuckHunt/blueDuckDead.png",
                 "/home/ludwigg/Python/PyRpi_DuckHunt/blackDuckDead.png",
                 "/home/ludwigg/Python/PyRpi_DuckHunt/redDuckDead.png")
+RoundDucks = []
+ActiveDucks = []
 duckIndex = 0
 
 aim = Circle(Point(250, 250), 15)
 innerAim = Circle(Point(250, 250), 5)
-target = Image(Point(0, 0), ducks[duckIndex])
-death = Image(Point(0, 0), deadDucks[duckIndex])
-message = Text(Point(100, 100), "")
+#target = Image(Point(0, 0), ducks[duckIndex])
+#death = Image(Point(0, 0), deadDucks[duckIndex])
+message = Text(Point(300, 100), "")
 scoreText = Text(Point(100, 50), "")
-score = 0
+roundText = Text(Point(100, 100), "")
+ducksMissedText = Text(Point(100, 150), "")
+shotsTakenText = Text(Point(100, 200), "")
+
+NUM_DUCKS_PER_PERIOD = 2
+NUM_PERIODS = 5
+NUM_DUCKS_PER_ROUND = NUM_DUCKS_PER_PERIOD * NUM_PERIODS
+TIME_TO_SHOOT = 6 # seconds
+TIME_BETWEEN_DUCKS = 1 # seconds
+TIME_BETWEEN_PERIODS = 1 # seconds
+TIME_BETWEEN_ROUNDS = 1 # seconds
+SHOTS_PER_PERIOD = 3
+shotsTakenInPeriod = 0
+roundNum = 0
+currentPeriod = 1
+totalScore = 0
+roundScore = 0
+ducksMissed = 0
+timeAtLastPeriod = 0
+lastPeriodSet = False
+timeAtLastRound = 0
+lastRoundSet = False
+centerX = round(SCREEN_WIDTH / 2)
+centerY = round(SCREEN_HEIGHT / 2)
+unDrawDuck = []
+kill = False
+
 playing = False
 
-win = GraphWin("Target Practice", SCREEN_WIDTH, SCREEN_HEIGHT, autoflush=False)
+win = GraphWin("Duck Hunt", SCREEN_WIDTH, SCREEN_HEIGHT, autoflush=False)
 
 def getXPosition():
     global chan
@@ -49,46 +78,145 @@ def getYPosition():
     global chan2
     return round(chan2.voltage/3.3 * SCREEN_HEIGHT)
     
-def spawnTarget():
-    global target
-    global kill
+def SetupRound():
     global ducks
-    global duckIndex
-    found = False
-    target.undraw()
-    duckIndex = random.randint(0,2)
-    while not found:
-        target = Image(Point(random.randint(20, SCREEN_WIDTH - 20), random.randint(20, SCREEN_HEIGHT - 20)), ducks[duckIndex])
-        targetCenter = target.getAnchor()
-        if math.sqrt((250 - targetCenter.x)**2 + (250 - targetCenter.y)**2) <= (260):
-            found = True
-    kill = False
-    target.draw(win)
+    global RoundDucks
+    global currentPeriod
+    global roundScore
+    global roundNum
+    global lastRoundSet
+    
+    lastRoundSet = False
+    currentPeriod = 1
+    roundScore = 0
+    roundNum += 1
+    
+    RoundDucks.clear()
+    #randIndex = random.randint(0,len(ducks) - 1)
+    for i in range(NUM_DUCKS_PER_ROUND):
+        randIndex = random.randint(0,len(ducks) - 1)
+        found = False
+        while not found:
+            tempDuck = Image(Point(random.randint(20, SCREEN_WIDTH - 20), random.randint(20, SCREEN_HEIGHT - 20)), ducks[randIndex])
+            targetCenter = tempDuck.getAnchor()
+            # TODO need to compute differently because not square screen
+            if math.sqrt((250 - targetCenter.x)**2 + (250 - targetCenter.y)**2) <= (260):
+                found = True
+                RoundDucks.append(Duck(tempDuck, i, 10, 10))
+    BeginPeriod()
+    
+def FlyAway():
+    global unDrawDuck
 
+    for duck in unDrawDuck:
+        duck.FlyingAway = True
+    
+def SpawnDuck(index):
+    global RoundDucks
+    global ActiveDucks
+    
+    RoundDucks[index].duckGraphic().draw(win)
+    RoundDucks[index].setActive()
+    ActiveDucks.append(RoundDucks[index])
+    
+def BeginPeriod():
+    global shotsTakenInPeriod
+    global RoundDucks
+    global ActiveDucks
+    global lastPeriodSet
+    
+    lastPeriodSet = False
+    index = 0
+    shotsTakenInPeriod = 0
+    duck = RoundDucks[index]
+    # loop till non-activated duck is found
+    while duck.isActivated():
+        index += 1
+        duck = RoundDucks[index]
+    
+    ActiveDucks.clear()
+    for i in range(NUM_DUCKS_PER_PERIOD):
+        SpawnDuck(index)
+        index += 1
+    
+# returns True when period is done
+def PeriodCheck():
+    global ActiveDucks
+    global timeAtLastPeriod
+    global currentPeriod
+    global lastPeriodSet
+    global ducksMissed
+    
+    isADuckActive = False
+    for duck in ActiveDucks:
+        if duck.isActive():
+            isADuckActive = True
+    if not isADuckActive and not lastPeriodSet:
+        for duck in ActiveDucks:
+                if duck.isAlive():
+                    ducksMissed += 1
+                    unDrawDuck.append(duck)
+                    FlyAway()
+        timeAtLastPeriod = time.time()
+        currentPeriod += 1
+        lastPeriodSet = True
+    return not isADuckActive
+    
+# returns true when round is done
+def RoundCheck():
+    global currentPeriod
+    global timeAtLastRound
+    global lastRoundSet
+    global lastPeriodSet
+    global unDrawDuck
+    
+    period = PeriodCheck()
+    if currentPeriod > NUM_PERIODS:
+        if not lastRoundSet:
+            for duck in ActiveDucks:
+                if duck.isAlive():
+                    unDrawDuck.append(duck)
+                    FlyAway()
+            timeAtLastRound = time.time()
+            lastRoundSet = True
+            
+        return True
+    else:
+        if period:
+            if time.time() - timeAtLastPeriod >= TIME_BETWEEN_PERIODS and len(unDrawDuck) == 0:
+                BeginPeriod()
+        return False
+    
 def shoot(channel):
-    global target
-    global kill
-    global score
+    global shotsTakenInPeriod
+    global RoundDucks
+    global roundScore
+    global totalScore
     global playing
+    global unDrawDuck
     
     if not playing:
         playing = True
     else:
-        aimCenter = aim.getCenter()
-        targetCenter = target.getAnchor()
-        
-        if math.sqrt((aimCenter.x - targetCenter.x)**2 + (aimCenter.y - targetCenter.y)**2) <= (30):
-            kill = True
-            score += 1
+        if shotsTakenInPeriod < SHOTS_PER_PERIOD:
+            aimCenter = aim.getCenter()
+            for target in ActiveDucks:
+                targetCenter = target.duckGraphic().getAnchor()
+                if math.sqrt((aimCenter.x - targetCenter.x)**2 + (aimCenter.y - targetCenter.y)**2) <= (30) and not target.FlyingAway:
+                    target.killed()
+                    unDrawDuck.append(target)
+                    roundScore += 1
+                    totalScore += 1
+            shotsTakenInPeriod += 1
 
 def main():
     global aim
     global innerAim
     global target
-    global kill
-    global score
-    global death
+    global totalScore
+    global unDrawDuck
     global playing
+    global ActiveDucks
     
     # set coordnate plane for easy translation from the joystick position
     # xll, yll, xur, yur
@@ -99,41 +227,73 @@ def main():
     message.setSize(20)
     message.draw(win)
     
+    roundText.setTextColor("white")
+    roundText.setSize(20)
+    roundText.draw(win)
+    
     scoreText.setTextColor("white")
     scoreText.setSize(20)
     scoreText.draw(win)
     
-    target.draw(win)
-    spawnTarget()
+    ducksMissedText.setTextColor("white")
+    ducksMissedText.setSize(20)
+    ducksMissedText.draw(win)
+    
+    shotsTakenText.setTextColor("white")
+    shotsTakenText.setSize(20)
+    shotsTakenText.draw(win)
+    
+    #target.draw(win)
+    #spawnTarget()
     
     aim.draw(win)
     innerAim.draw(win)
     
-    dog = Image(Point(250,250), "/home/ludwigg/Python/PyRpi_DuckHunt/dog.png")
+    dog = Image(Point(centerX,centerY), "/home/ludwigg/Python/PyRpi_DuckHunt/dog.png")
     
-    end = time.time() + 30
-    deathTime = 0
+    SetupRound()
     while not playing: # need to press button to begin
         deathTime = 0 # need something so loop runs
     while(playing):
-        timeLeft = round(end - time.time(), 2)
-        if timeLeft <= 0:
+        #timeLeft = round(end - time.time(), 2)
+        if ducksMissed >= 10:
             message.setText("Game Over!")
-            scoreText.setText("Final Score: " + str(score))
+            scoreText.setText("Final Score: " + str(totalScore))
             dog.draw(win)
             playing = False
         else:
-            message.setText(timeLeft)
-            scoreText.setText("Score: " + str(score))
-            if kill:
+            #message.setText(timeLeft)
+            scoreText.setText("Score: " + str(totalScore))
+            roundText.setText("Round: " + str(roundNum))
+            ducksMissedText.setText("Ducks Missed: " + str(ducksMissed))
+            shotsTakenText.setText("Shots Left: " + str(SHOTS_PER_PERIOD - shotsTakenInPeriod))
+            
+            if RoundCheck():
+                if time.time() - timeAtLastRound > TIME_BETWEEN_ROUNDS and len(unDrawDuck) == 0:
+                    SetupRound()
+            
+            CanClear = True
+            for unDraw in unDrawDuck:
                 try:
-                    death.undraw()
+                    #duckCenter = unDraw.duckGraphic().getAnchor()
+                    if not unDraw.FlyingAway:
+                        unDraw.duckGraphic().undraw()
+                    
+                    elif unDraw.duckGraphic().getAnchor().y > SCREEN_HEIGHT + 20:
+                        unDraw.FlyingAway = False
+                        unDraw.duckGraphic().undraw()
+                    else:
+                        unDraw.duckGraphic().move(0, 8)
+                        CanClear = False
                 except:
-                    print()
-                death = Image(target.getAnchor(), deadDucks[duckIndex])
-                spawnTarget()
-                death.draw(win)
-                deathTime = time.time() + .5
+                    None
+            if CanClear:
+                unDrawDuck.clear()
+                # death = Image(target.getAnchor(), deadDucks[duckIndex])
+                # spawnTarget()
+                # death.draw(win)
+                # deathTime = time.time() + .5
+            
                 
             aim.undraw()
             innerAim.undraw()
@@ -147,12 +307,26 @@ def main():
             aim.draw(win)
             innerAim.draw(win)
             
-            if (deathTime - time.time()) < 0:
-                try:
-                    death.undraw()
-                except:
-                    print()
+            for duck in ActiveDucks:
+                if duck.isActive():
+                    duckCenter = duck.duckGraphic().getAnchor()
+                    if duckCenter.x >= SCREEN_WIDTH - 30:
+                        duck.setVelocityX(-abs(duck.getRawVelocityX()))
+                    elif duckCenter.x <= 30:
+                        duck.setVelocityX(abs(duck.getRawVelocityX()))
+
+                    if duckCenter.y >= SCREEN_HEIGHT - 30:
+                        duck.setVelocityY(-abs(duck.getRawVelocityY()))
+                    elif duckCenter.y <= 30:
+                        duck.setVelocityY(abs(duck.getRawVelocityY()))
+                        
+                    duck.duckGraphic().move(duck.getVelocityX(), duck.getVelocityY())
             
+            # if (deathTime - time.time()) < 0:
+                # try:
+                    # death.undraw()
+                # except:
+                    # print()
         update(60)
     
     time.sleep(5)
